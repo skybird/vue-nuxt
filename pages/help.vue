7ec0981f91f41a95243d040b1e4478c05ead41aa
余天 <623247130@qq.com>
2017-11-04 14:42:44 +0800

    import resources to project

---
+++ pages/help.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="page-help">
+    this is help page
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+@import "~assets/scss/help";
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="wrap">
+    <div class="main">
+      <comLogo />
+      <com-logo></com-logo>
+      <h1 class="title">vue-nuxt</h1>
+      <h2 class="subtitle">Nuxt.js project</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+// require('~/assets/scss/index.scss')  // 不推荐这种方式，存在闪屏，在页面渲染完成后再加载css
+export default {
+  components: {
+    comLogo: Logo
+  }
+}
+</script>
+
+<style lang="scss">
+@import "~assets/scss/index";
+</style>
